193a1793c183b70a9757e168cea85bba96e30d5c
Xiaocheng Wei <xiaocheng.wei@sap.com>
2019-11-29 11:19:04 +0800

    Initial code

---
+++ src/test/java/com/sap/ase/LeapYearsTest.java
@@ -0,0 +1,12 @@
+package com.sap.ase;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LeapYearsTest {
+
+	@Test
+	public void testName() throws Exception {
+		fail("Not yet implemented");
+	}
+}
